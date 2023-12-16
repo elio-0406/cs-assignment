1b543348e2e48d4977b52f0990817dbe3d3e1b20
elio_0406 <stsupermarik@gmail.com>
2023-12-16 20:18:04 +0700

    Xử lý các sự kiện không liên quan hệ thống form đăng nhập

--- GUI/FormChinh/DangNhapForm.Designer.cs
+++ GUI/FormChinh/DangNhapForm.Designer.cs
@@ -28,21 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tenDangNhapTextBox = new TextBox();
+            matKhauTextBox = new TextBox();
+            dangNhapButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
+            // 
+            // tenDangNhapTextBox
+            // 
+            tenDangNhapTextBox.Location = new Point(232, 63);
+            tenDangNhapTextBox.Name = "tenDangNhapTextBox";
+            tenDangNhapTextBox.Size = new Size(150, 31);
+            tenDangNhapTextBox.TabIndex = 0;
+            tenDangNhapTextBox.KeyPress += TenDangNhapTextBox_KeyPress;
+            // 
+            // matKhauTextBox
+            // 
+            matKhauTextBox.Location = new Point(232, 101);
+            matKhauTextBox.Name = "matKhauTextBox";
+            matKhauTextBox.Size = new Size(150, 31);
+            matKhauTextBox.TabIndex = 1;
+            matKhauTextBox.UseSystemPasswordChar = true;
+            matKhauTextBox.KeyPress += MatKhauTextBox_KeyPress;
+            matKhauTextBox.MouseLeave += MatKhauTextBox_MouseLeave;
+            matKhauTextBox.MouseMove += MatKhauTextBox_MouseMove;
+            // 
+            // dangNhapButton
+            // 
+            dangNhapButton.Location = new Point(173, 149);
+            dangNhapButton.Name = "dangNhapButton";
+            dangNhapButton.Size = new Size(112, 34);
+            dangNhapButton.TabIndex = 2;
+            dangNhapButton.Text = "Đăng nhập";
+            dangNhapButton.UseVisualStyleBackColor = true;
+            dangNhapButton.Click += DangNhapButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(77, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Tên đăng nhập";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(77, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Mật khẩu";
             // 
             // DangNhapForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(459, 247);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dangNhapButton);
+            Controls.Add(matKhauTextBox);
+            Controls.Add(tenDangNhapTextBox);
             KeyPreview = true;
             MaximizeBox = false;
             Name = "DangNhapForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Đăng nhập";
+            KeyDown += DangNhapForm_KeyDown;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tenDangNhapTextBox;
+        private TextBox matKhauTextBox;
+        private Button dangNhapButton;
+        private Label label1;
+        private Label label2;
     }
 }
--- GUI/FormChinh/DangNhapForm.cs
+++ GUI/FormChinh/DangNhapForm.cs
@@ -16,5 +16,42 @@ namespace GUI.FormChinh
         {
             InitializeComponent();
         }
+
+        private void DangNhapButton_Click(object sender, EventArgs e)
+        {
+            new TrangChuForm().ShowDialog();
+            this.Hide();
+        }
+
+        private void TenDangNhapTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && (e.KeyChar < 32 || e.KeyChar > 126 || e.KeyChar == ' '))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MatKhauTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && (e.KeyChar < 32 || e.KeyChar > 126 || e.KeyChar == ' '))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MatKhauTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            matKhauTextBox.UseSystemPasswordChar = true;
+        }
+
+        private void MatKhauTextBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            matKhauTextBox.UseSystemPasswordChar = false;
+        }
+
+        private void DangNhapForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            DangNhapButton_Click(sender, e);
+        }
     }
 }
