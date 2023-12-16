7443959d6463317c8679448c828fb6f9d8ba180b
elio_0406 <stsupermarik@gmail.com>
2023-12-16 13:29:33 +0700

    Tạo các file ban đầu

---
+++ GUI/FormChiTiet/ChiTietKhuyenMaiForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.FormChiTiet
+{
+    public partial class ChiTietKhuyenMaiForm : Form
+    {
+        public ChiTietKhuyenMaiForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- GUI/Program.cs
+++ GUI/Program.cs
@@ -1,4 +1,6 @@
-namespace cs_assignment
+using GUI.FormChinh;
+
+namespace GUI
 {
     internal static class Program
     {
@@ -11,7 +13,7 @@ namespace cs_assignment
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new DangNhapForm());
         }
     }
 }
