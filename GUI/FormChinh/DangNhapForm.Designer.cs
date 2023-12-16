810c129f353f281bc64fbc2c600c60d078d65948
elio_0406 <stsupermarik@gmail.com>
2023-12-16 14:07:52 +0700

    Tạo các control cần thiết, định dạng nhập text box cho form đăng nhập

--- GUI/FormChinh/DangNhapForm.Designer.cs
+++ GUI/FormChinh/DangNhapForm.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DangNhapForm";
+            dangNhapButton = new Button();
+            label2 = new Label();
+            tenDangNhapTextBox = new TextBox();
+            matKhauTextBox = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // dangNhapButton
+            // 
+            dangNhapButton.Anchor = AnchorStyles.None;
+            dangNhapButton.AutoSize = true;
+            dangNhapButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            dangNhapButton.BackColor = Color.White;
+            dangNhapButton.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dangNhapButton.Location = new Point(144, 154);
+            dangNhapButton.Name = "dangNhapButton";
+            dangNhapButton.Padding = new Padding(5);
+            dangNhapButton.Size = new Size(177, 58);
+            dangNhapButton.TabIndex = 10;
+            dangNhapButton.Text = "Đăng nhập";
+            dangNhapButton.UseVisualStyleBackColor = false;
+            dangNhapButton.Click += dangNhapButton_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(86, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 25);
+            label2.TabIndex = 14;
+            label2.Text = "Mật khẩu";
+            // 
+            // tenDangNhapTextBox
+            // 
+            tenDangNhapTextBox.Anchor = AnchorStyles.None;
+            tenDangNhapTextBox.BackColor = Color.White;
+            tenDangNhapTextBox.BorderStyle = BorderStyle.None;
+            tenDangNhapTextBox.Location = new Point(261, 35);
+            tenDangNhapTextBox.Name = "tenDangNhapTextBox";
+            tenDangNhapTextBox.Size = new Size(150, 24);
+            tenDangNhapTextBox.TabIndex = 11;
+            // 
+            // matKhauTextBox
+            // 
+            matKhauTextBox.Anchor = AnchorStyles.None;
+            matKhauTextBox.BorderStyle = BorderStyle.None;
+            matKhauTextBox.Location = new Point(261, 90);
+            matKhauTextBox.Name = "matKhauTextBox";
+            matKhauTextBox.Size = new Size(150, 24);
+            matKhauTextBox.TabIndex = 12;
+            matKhauTextBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(86, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 25);
+            label1.TabIndex = 13;
+            label1.Text = "Tên đăng nhập";
+            // 
+            // DangNhapForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(459, 247);
+            Controls.Add(dangNhapButton);
+            Controls.Add(label2);
+            Controls.Add(tenDangNhapTextBox);
+            Controls.Add(matKhauTextBox);
+            Controls.Add(label1);
+            KeyPreview = true;
+            MaximizeBox = false;
+            Name = "DangNhapForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng nhập";
+            KeyDown += DangNhapForm_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button dangNhapButton;
+        private Label label2;
+        private TextBox tenDangNhapTextBox;
+        private TextBox matKhauTextBox;
+        private Label label1;
     }
 }
--- GUI/FormChinh/DangNhapForm.cs
+++ GUI/FormChinh/DangNhapForm.cs
@@ -16,5 +16,31 @@ namespace GUI.FormChinh
         {
             InitializeComponent();
         }
+
+        private void tenDangNhapTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && e.KeyChar > 127 || e.KeyChar == ' ')
+            {
+                e.Handled = true; 
+            }
+        }
+
+        private void matKhauTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && e.KeyChar > 127 || e.KeyChar == ' ')
+            {
+                e.Handled = true; 
+            }
+        }
+
+        private void dangNhapButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DangNhapForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            dangNhapButton_Click(sender, e);
+        }
     }
 }
